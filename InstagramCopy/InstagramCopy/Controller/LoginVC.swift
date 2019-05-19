dfa9bbc6289e2c184e0208dd17bf1047ddc75a13
92pino <jjb8382@gmail.com>
2019-05-19 12:41:26 +0900

    instagramcopy init

--- InstagramCopy/InstagramCopy/Controller/LoginVC.swift
+++ InstagramCopy/InstagramCopy/Controller/LoginVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class LoginVC: UIViewController {
-  
+    
     let emailTextField: UITextField = {
         let tf = UITextField()
         tf.placeholder = "Email"
@@ -20,32 +20,8 @@ class LoginVC: UIViewController {
         return tf
     }()
     
-    let passwordTextField: UITextField = {
-        let tf = UITextField()
-        tf.placeholder = "Password"
-        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
-        tf.borderStyle = .roundedRect
-        tf.font = UIFont.systemFont(ofSize: 14)
-        
-        return tf
-    }()
-    
-    let loginButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Sign Up", for: .normal)
-        button.backgroundColor = UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: 1)
-        
-        return button
-    }()
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // background Color
-        view.backgroundColor = .white
-        
-        view.addSubview(emailTextField)
-        emailTextField.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 40, paddingLeft: 20, paddingBottom: 0, paddingRight: 20, width: 0, height: 40)
     }
-
+    
 }
