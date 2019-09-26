082a06c1aea169163952053126d7fc9250017182
92pino <jjb8382@gmail.com>
2019-09-26 18:46:43 +0900

    docs : 초기 화면 셋팅 및 README.md 추가

---
+++ iPhone_weatherApp/README.md
@@ -0,0 +1,21 @@
+
+# Apple Weather App Clone Project
+
+> 아이폰 날씨 어플 Clone 프로젝트
+
+## Commit Message 규칙
+- ```feat``` : 새로운 기능 추가
+- ```fix``` : 버그 수정
+- ```docs``` : 문서 수정
+- ```style``` : 파일 그룹화, 세미콜론 누락, 코드 변경이 없는 경우
+- ```refactor``` : 코드 리펙토링
+- ```test``` : 테스트 코드, 리펙토링 테스트 코드 추가
+
+## Style Guide
+- Swift Dev Version : 5
+- Deployment Target : 12.2
+- Device Target : iPhone
+- Code Architecture : MVC
+
+## Reference
+[Dark Sky](https://darksky.net/dev/docs)
---
+++ iPhone_weatherApp/iPhone_weatherApp/AppDelegate/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  iPhone_weatherApp
+//
+//  Created by JinBae Jeong on 2019/09/26.
+//  Copyright © 2019 pino. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        
+        window?.rootViewController = <#viewController#>
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
